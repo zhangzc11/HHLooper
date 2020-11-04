d36f4cb9fa27307777e8ab4b1fcad2eeb9bd4574
Zhicai <zzhang2@caltech.edu>
2020-11-04 15:08:51 -0600

    update

--- python/plot_all.sh
+++ python/plot_all.sh
@@ -1,25 +1,23 @@
 
 TAG=$1
 
+
 for region in \
-CutBin1 \
-CutBin2 \
-CutBin3 \
-CutBin4 \
+FitCR1  \
+FitCR2  \
+FailFitCR1  \
+FailFitCR2  \
+FailSR  
+do
+    python plot.py -i ../hists/${TAG}/combine/ -w ${region} -n 20 -d
+done
+
+for region in \
 SRBin1 \
 SRBin2 \
 SRBin3 \
 SRBin4 
 do
-    python plot.py -i ../hists/${TAG}/combine/ -s 10 -w ${region} -n 40
-done
-
-for region in \
-J2MassSideBandBin1 \
-J2MassSideBandBin2 \
-J2MassSideBandBin3 \
-J2MassSideBandBin4 
-do
-    python plot.py -i ../hists/${TAG}/combine/ -w ${region} -n 40 -d
+    python plot.py -i ../hists/${TAG}/combine/ -s 10 -w ${region} -n 20
 done
 
