8c92295e1e85858c5af714931f55e92483057040
Nan Lu <nan.lu@cern.ch>
2021-06-28 19:16:39 -0700

    update instructions

--- run_syst_test.sh
+++ run_syst_test.sh
@@ -1,10 +1,10 @@
 #outputname no/yesforsyst systname no/yestrigsys no/yesPNetSFsys
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_nominal_nosys no nominal no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JES_Up no JES_Up no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JES_Down no JES_Down no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JMR_Up no JMR_Up no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JMR_Down no JMR_Down no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JMS_Up no JMS_Up no no
-source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_JMS_Down no JMS_Down no no
-#source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_nominal yes nominal no yes
-#source process.sh yield_AN_sr_sys_0604BaselineNewTTcorPNetV2_trig_nominal yes nominal yes no
+source process.sh yield_AN_sr_sys_0624_nominal_nosys no nominal no no
+source process.sh yield_AN_sr_sys_0624_JES_Up no JES_Up no no
+source process.sh yield_AN_sr_sys_0624_JES_Down no JES_Down no no
+source process.sh yield_AN_sr_sys_0624_JMR_Up no JMR_Up no no
+source process.sh yield_AN_sr_sys_0624_JMR_Down no JMR_Down no no
+source process.sh yield_AN_sr_sys_0624_JMS_Up no JMS_Up no no
+source process.sh yield_AN_sr_sys_0624_JMS_Down no JMS_Down no no
+source process.sh yield_AN_sr_sys_0624_nominal yes nominal no yes
+#source process.sh yield_AN_sr_sys_0624_trig_nominal yes nominal yes no
